f563c66f7bc34afea5cc9d158b30bc9996c79b11
jahidhasan8 <jahidhasan1932@gmail.com>
2022-09-14 23:07:34 +0600

    start countdown debug done

--- script.js
+++ script.js
@@ -107,7 +107,7 @@ const start = () => {
   countdownOverlay.style.display = "flex";
 
   const startCountdown = setInterval(() => {
-    countdownOverlay.innerHTML = '<h1>${count}</h1>';
+    countdownOverlay.innerHTML = `<h1>${count}</h1>`;
 
     // finished timer
     if (count == 0) {
@@ -124,6 +124,7 @@ const start = () => {
 };
 
 // START Countdown
+// startBtn.addEventListener("click", start);
 startBtn.addEventListener("click", start);
 
 // If history exists, show it
