64153dcd916000f14261c13c2318b66d0d062131
jahidhasan8 <jahidhasan1932@gmail.com>
2022-09-15 14:13:11 +0600

    in history part typing time converted to integer value in seconds

--- script.js
+++ script.js
@@ -68,7 +68,7 @@ const gameOver = () => {
   // the current time is the finish time
   // so total time taken is current time - start time
   const finishTime = new Date().getTime();
-  const timeTaken = (finishTime - startTime) / 1000;
+  const timeTaken = parseInt((finishTime - startTime) / 1000);
 
   // show result modal
   resultModal.innerHTML = "";
@@ -78,10 +78,10 @@ const gameOver = () => {
   display.innerHTML = "";
   // make it inactive
   display.classList.add("inactive");
-  
+
   // modalBackground.style.display = "flex";
   // show result
-  
+
   resultModal.innerHTML += `
   
     <h1>Finished!</h1>
@@ -115,14 +115,14 @@ const start = () => {
     countdownOverlay.innerHTML = `<h1>${count}</h1>`;
 
     // finished timer
-    if (count == 0) { 
+    if (count == 0) {
       // -------------- START TYPING -----------------
       document.addEventListener("keydown", typeController);
       countdownOverlay.style.display = "none";
       display.classList.remove("inactive");
 
       clearInterval(startCountdown);
-     startTime = new Date().getTime();
+      startTime = new Date().getTime();
     }
     count--;
   }, 1000);
@@ -137,7 +137,7 @@ displayHistory();
 // Show typing time spent
 setInterval(() => {
   const currentTime = new Date().getTime();
-  const timeSpent =parseInt((currentTime - startTime) / 1000) ;
+  const timeSpent = parseInt((currentTime - startTime) / 1000);
 
 
   document.getElementById("show-time").innerHTML = `${startTime ? timeSpent : 0} seconds`;
